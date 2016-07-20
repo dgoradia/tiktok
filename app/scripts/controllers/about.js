17a1e9038ca36ea9c015825dd10789c473910ec0
Dru Goradia <dru@atlasdigital.tv>
2016-07-20 15:55:57 -0700

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tiktokFitnessApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tiktokFitnessApp
+ */
+angular.module('tiktokFitnessApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tiktokFitnessApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tiktokFitnessApp
+ */
+angular.module('tiktokFitnessApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
