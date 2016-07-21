0c86bcffe4aee7fcbc5aa1991b3034572eebd34b
Dru Goradia <dru@atlasdigital.tv>
2016-07-21 12:52:30 -0700

    Images preloading

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,23 @@
  * Controller of the tiktokFitnessApp
  */
 angular.module('tiktokFitnessApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function (preloader) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    this.imageLocations = [
+        'images/training-train-lime-barbell-39688.jpeg',
+        'images/logo.png',
+        'images/large.jpg'
+    ];
+
+    preloader.preloadImages(this.imageLocations)
+        .then(function(resp) {
+            console.log(resp);
+        }, function(error) {
+            console.log(error);
+        });
   });
