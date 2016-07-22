2400dad2956383b4b7d0a1f9de20f71d32b9d778
Dru Goradia <dru@atlasdigital.tv>
2016-07-22 12:31:24 -0700

    Video background

--- app/scripts/controllers/training.js
+++ app/scripts/controllers/training.js
@@ -9,9 +9,4 @@
  */
 angular.module('tiktokFitnessApp')
   .controller('TrainingCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
