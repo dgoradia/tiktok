0c3ec86df3d6101bdd867ad1a6e7bcdf04bc33f9
Dru Goradia <dru@atlasdigital.tv>
2016-07-22 17:23:14 -0700

    Revert "Adds lightwight server"
    This reverts commit 37527e2af7094c14a1ce63aad5c41538927d530f.

--- app/server.js
+++
@@ -1,8 +0,0 @@
-'use-strict';
-
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-connect().use(serveStatic(__dirname + '../dist')).listen(80, function() {
-    console.log('Server running on 80...');
-});
